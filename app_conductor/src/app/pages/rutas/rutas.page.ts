fc55e037fbfdfb373aae582505f79b68374d7ef8
jhonnymunoz97 <jhonnyjmc97@gmail.com>
2021-06-13 16:10:34 -0500

    Implementado geolocalización del conductor

--- app_conductor/src/app/pages/rutas/rutas.page.ts
+++ app_conductor/src/app/pages/rutas/rutas.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 //import { from } from 'rxjs';
 
 import {ViewChild, ElementRef} from '@angular/core';
 import { Ruta } from 'src/app/models/ruta';
 import { RutaService } from 'src/app/service/ruta.service';
+import {Geolocation} from '@capacitor/geolocation';
 /* import {
   CameraPosition,
   GoogleMap,
@@ -31,7 +32,7 @@ declare var google: any;
 })
 
 
-export class RutasPage implements OnInit {
+export class RutasPage implements OnInit, DoCheck {
 
 
   rutas: Ruta[];
@@ -40,14 +41,43 @@ export class RutasPage implements OnInit {
   directionsServices = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer();
 
-
+  //@Input() title:string;
  
   @ViewChild('map',{read: ElementRef, static:false}) mapRef: ElementRef;
 
-  constructor(private rutaService: RutaService) { }
+  constructor(private rutaService: RutaService) {
+    this.option_tour = false
+   }
+
+  start_tour: boolean = false
+  
+  inicio:boolean = false
+  fin: boolean = false
+
+  ngDoCheck(): void {
+    //throw new Error('Method not implemented.');
+    /* if(this.start_tour){
+      setTimeout(this.getLocation, 5000)
+    }else{
+      if(this.fin) console.log('Fin del recorrido')
+      //console.log("Fin de geolocalización")
+      //window.location.reload();
+    } */
+    
+    //console.log('doCheak')
+
+    if(this.inicio) setTimeout(this.getLocation, 5000)
+    if(this.fin) window.location.reload();
+  }
+  
+  /* ngOnChanges(changes: SimpleChanges): void {
+    //throw new Error('Method not implemented.');
+    console.log(changes)
+  } */
 
   ngOnInit() {
     this.getRutas()
+    //this.title = 'Hola'
   }
 
   alerta(){
@@ -105,6 +135,36 @@ export class RutasPage implements OnInit {
 
 onChange($event) {
   this.paintRoute(parseInt($event.target.value));
+}
+
+
+//start_tour: boolean = false
+setLocation(start: boolean){
+  /* this.start_tour = start 
+  if(!this.start_tour) this.fin = true */
+  if(start) this.inicio = true
+  else this.fin = true
+}
+
+option_tour:boolean
+
+async getLocation(){
+  const coordenadas = await Geolocation.getCurrentPosition() 
+  let latitude = coordenadas.coords.latitude
+  let longitude = coordenadas.coords.longitude
+  console.log("La latitud es: " + latitude)
+  console.log("La longitud es: " + longitude)
+  //}else{
+  /* if(this.start_tour){
+    const coordenadas = await Geolocation.getCurrentPosition() 
+    let latitude = coordenadas.coords.latitude
+    let longitude = coordenadas.coords.longitude
+    console.log("La latitud es: " + latitude)
+    console.log("La longitud es: " + longitude)
+  }else{
+    
+  } */
+  ///this.title = 'jajajja'
 }
 
 }
